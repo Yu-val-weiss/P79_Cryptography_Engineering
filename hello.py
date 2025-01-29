50770d72a97788d4753e2b0b31feded4d078d1fa
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2025-01-29 12:47:02 +0000

    add pynacl

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
-from lab1 import hello
+from hashlib import sha256  # Python standard library
+
+from lab1 import hello as hello1
 from lab2 import hello as hello2
 from lab3 import hello as hello3
 
 
 def main():
     print("Hello from p79-cryptography-engineering!")
-    print(hello())
-    print(hello2())
-    print(hello3())
+    print(sha256(hello1().encode("utf-8")).hexdigest())
+    print(sha256(hello2().encode("utf-8")).hexdigest())
+    print(sha256(hello3().encode("utf-8")).hexdigest())
 
 
 if __name__ == "__main__":
