49d1e767747acd1563beb078c4e08d63664763a5
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2025-01-29 16:58:16 +0000

    Update run.sh
    - use cache in build
    - rename to lab0_runner

--- lab0/run.sh
+++ lab0/run.sh
@@ -5,7 +5,7 @@ set -e;
 cd "$(dirname "$0")"
 
 # Build the Docker image
-docker build --no-cache -t p79-runner .
+docker build -t lab0_runner .
 
 # Run the container
-docker run --rm p79-runner
+docker run --rm lab0_runner
