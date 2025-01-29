1bd82382fa65f56645d638d429272591617db150
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2025-01-29 12:39:46 +0000

    initialise uv

---
+++ hello.py
@@ -0,0 +1,14 @@
+from lab1 import hello
+from lab2 import hello as hello2
+from lab3 import hello as hello3
+
+
+def main():
+    print("Hello from p79-cryptography-engineering!")
+    print(hello())
+    print(hello2())
+    print(hello3())
+
+
+if __name__ == "__main__":
+    main()
